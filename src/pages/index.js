cc68916e1c115bb5d2af9278fd75af7ef48ef5b2
Wowosz <wojciech.szodaa@gmail.com>
2020-08-11 17:57:28 +0200

    added some logos

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,10 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import image from '../images/main-img.jpg'
-import john from '../images/john-deer.png'
+import john from '../images/john-deer.jpg'
 import zetor from '../images/zetor.jpg'
+import massey from '../images/massey-ferguson-logo.jpg'
+import landini from '../images/landini-logo-png-transparent.png'
 import '../styles/css/bootstrap.min.css';
 import '../styles/index.css'
 
@@ -20,6 +22,8 @@ const IndexPage = () => (
         <div className="row brandRow p-3 d-flex justify-content-center">
           <img src={john} alt="john Deer logo" className="img-fluid h-100 mx-1"></img>
           <img src={zetor} alt="zetor logo" className="img-fluid h-100 mx-1"></img>
+          <img src={massey} alt="massey logo" className="img-fluid h-100 mx-1"></img>
+          <img src={landini} alt="zetor logo" className="img-fluid h-100 mx-1"></img>
         </div>
         <div className="row"></div>
         <div className="row"></div>
