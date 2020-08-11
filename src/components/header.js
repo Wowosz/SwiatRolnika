1a2656c16e34f86b6058d5771cf83aafd5458196
Wowosz <wojciech.szodaa@gmail.com>
2020-08-11 20:24:36 +0200

    still contacts

--- src/components/header.js
+++ src/components/header.js
@@ -19,7 +19,7 @@ const Header = () => {
             <div className={opened ? 'menu-btn open' : 'menu-btn'}  onClick={handleClick}>
                 <div className="menu-btn__burger"></div>
             </div>
-            <div className={`${opened ? 'mobileLinkListContainer open' : 'mobileLinkListContainer'} d-flex align-items-center justify-content-center`}>
+            <div className={`${opened ? 'mobileLinkListContainer open' : 'mobileLinkListContainer'} align-items-center justify-content-center`}>
                 <ul className="mobileLinkList p-0 text-center">
                     <li className="my-4"><a href="/"><h1>Start</h1></a></li>
                     <li className="my-4"><a href="/o-nas"><h1>O Nas</h1></a></li>
@@ -30,12 +30,12 @@ const Header = () => {
         </div>
 
         <div className="desktopNavBar container-fluid flex-row-reverse mx-0">
-            <div className="col-lg-8 ">
+            <div className="col-lg-8 col-md-8">
                 <ul className="navBarList row  justify-content-center m-0 h-100">
-                    <Link to="/"  className="px-lg-3 text-white d-flex align-items-center text-decoration-none"><li><h5>Start</h5></li></Link>
-                    <Link to="/o-nas/"  className="px-lg-3 text-white d-flex align-items-center text-decoration-none"><li><h5>O nas</h5></li></Link>
-                    <Link to="/oferta/"  className="px-lg-3 text-white d-flex align-items-center text-decoration-none"><li><h5>Oferta</h5></li></Link>
-                    <Link to="/kontakt/"  className="px-lg-3 text-white d-flex align-items-center text-decoration-none"><li><h5>Kontakt</h5></li></Link>
+                    <Link to="/"  className="px-lg-3 px-md-2 text-white d-flex align-items-center text-decoration-none"><li><h5>Start</h5></li></Link>
+                    <Link to="/o-nas"  className="px-lg-3 px-md-2 text-white d-flex align-items-center text-decoration-none"><li><h5>O nas</h5></li></Link>
+                    <Link to="/oferta"  className="px-lg-3 px-md-2 text-white d-flex align-items-center text-decoration-none"><li><h5>Oferta</h5></li></Link>
+                    <Link to="/kontakt"  className="px-lg-3 px-md-2 text-white d-flex align-items-center text-decoration-none"><li><h5>Kontakt</h5></li></Link>
                 </ul>
             </div>
         </div>
--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -12,6 +12,20 @@ const Kontakt = () => {
             </div>
             <div className="shadowUnder"></div>
         </div>
+        <div className="row mt-5">
+            <div className="col-lg-6 px-5 py-3 col-10 col-md-8 embed-responsive-item">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d511.0937971074316!2d23.35372347301636!3d53.654486889074974!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x278b0856041ee498!2sHandlowo-Techniczne%20Us%C5%82ugi%20Rolnicze!5e1!3m2!1spl!2spl!4v1597166969403!5m2!1spl!2spl" frameborder="0"  allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+            <div className="col-lg-6 px-5 py-3 col-10 col-md-8">
+                <p>Handlowo Techniczne Usługi Rolnicze Sp.j. Harackiewicz,Sidor</p>
+                <p>ul.Obwodowa 6 16-200 Dąbrowa Białostocka</p>
+                <p>Tel. (85) 71 22 195</p>
+                <p>Kom. 602 348 161</p>
+                <p>e-mail: htur_sj.dabrowa@wp.pl</p>
+                <p>Pn-pt: 7-17</p>
+                <p>Sob: 7-14</p>
+            </div>
+        </div>
     </Layout>
     )
 }
