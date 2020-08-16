1035f3f60a5acc16928ae541e9418e622bd1ac84
Wowosz <wojciech.szodaa@gmail.com>
2020-08-16 13:11:14 +0200

    .

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return(
         <div className="containe-fluid row align-content-center bg-secondary pt-4 flex-column text-center text-white mx-0">
             <h5>©2020,Handlowo Techniczne Usługi Rolnicze</h5>
-            <p>Created by <a className="text-decoration-none link" href="https://www.facebook.com/wojtek.szoda.3/">Wojciech Szoda</a></p>
+            <p className="credit">Created by <a className="text-decoration-none link" href="https://www.facebook.com/wojtek.szoda.3/">Wojciech Szoda</a></p>
         </div>
     );
 }
