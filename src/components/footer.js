28e69e5f5e2981d99e3def3aedae11dc5de152c3
Wowosz <wojciech.szodaa@gmail.com>
2020-09-21 09:34:49 +0200

    .

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import "./footer.css"
 const Footer = () => {
     return(
         <div className="containe-fluid row align-content-center bg-secondary pt-4 flex-column text-center text-white mx-0">
-            <h5>©2020,Handlowo Techniczne Usługi Rolnicze</h5>
+            <h5>©2020, Handlowo Techniczne Usługi Rolnicze Sp.j. Harackiewicz,Sidor</h5>
             <p className="credit">Created by <a className="text-decoration-none link" href="https://www.facebook.com/wojtek.szoda.3/">Wojciech Szoda</a></p>
         </div>
     );
